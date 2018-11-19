20e0fa3ab62ebd9e11642ca85cbe8448d72225a0
Elyes Turki <elyestu@gmail.com>
2018-11-19 16:49:23 +0100

    Add css class to #header on Scrolling with typescript and no jQuery

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,31 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+// import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  // tslint:disable-next-line:use-host-property-decorator
+  host: {
+    '(window:scroll)': 'onScroll($event)'
+  }
 })
 export class HeaderComponent implements OnInit {
+  isScrolled = false;
+  currPos: Number = 0;
+  startPos: Number = 0;
+  changePos: Number = 100;
+  constructor() {
 
-  constructor()  {
-    this.headerScroll();
   }
 
   ngOnInit() {
-
+    console.log('ngOnInit32');
   }
 
-  headerScroll() {
-    console.log("document.body.scrollTop: ", document.body.scrollTop);
-    document.getElementById("header").addClass = "header-scrolled";
-    if (document.body.scrollTop > 100) {
-     // document.getElementById("header").addClass = "header-scrolled";
-  } else {
-     // document.getElementById("header").removeClass = "header-scrolled";
-  }
-  }
-
-
+  onScroll(evt) {
+    this.currPos = (window.pageYOffset || evt.target.scrollTop) - (evt.target.clientTop || 0);
+    if(this.currPos >= this.changePos ) {
+        this.isScrolled = true;
+    } else {
+        this.isScrolled = false;
+    }
+}
 
 }
 
