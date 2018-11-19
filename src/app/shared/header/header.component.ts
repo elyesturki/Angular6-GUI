c55b466e4fc20fca559bcc4327a7f5db26e98245
Elyes Turki <elyestu@gmail.com>
2018-11-19 10:34:29 +0100

    Add header and footer css/js

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor()  {
+    this.headerScroll();
   }
 
+  ngOnInit() {
+
+  }
+
+  headerScroll() {
+    console.log("document.body.scrollTop: ", document.body.scrollTop);
+    document.getElementById("header").addClass = "header-scrolled";
+    if (document.body.scrollTop > 100) {
+     // document.getElementById("header").addClass = "header-scrolled";
+  } else {
+     // document.getElementById("header").removeClass = "header-scrolled";
+  }
+  }
+
+
+
 }
+
+// Header scroll class
+// $(window).scroll(function() {
+//   if ($(this).scrollTop() > 100) {
+//       $('#header').addClass('header-scrolled');
+//   } else {
+//       $('#header').removeClass('header-scrolled');
+//   }
+// });
