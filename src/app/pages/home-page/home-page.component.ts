e760f878e36a81d3c4ae37b9d6621a24f1c34d57
Elyes Turki <elyestu@gmail.com>
2018-11-28 15:59:47 +0100

    Create list albumId select-option in Homepage

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -3,8 +3,9 @@ import { Observable, of, throwError } from 'rxjs';
 import { HttpClient, HttpHeaders, HttpErrorResponse, HttpHeaderResponse, HttpParams } from '@angular/common/http';
 
 import {CanActivate, ActivatedRouteSnapshot, Router, RouterStateSnapshot} from '@angular/router';
- 
 
+//import service ListProductsService
+import  { ListProductsService } from '../../services/list-products.service';
 
 @Component({
   selector: 'app-home-page',
@@ -13,25 +14,38 @@ import {CanActivate, ActivatedRouteSnapshot, Router, RouterStateSnapshot} from '
 })
 export class HomePageComponent implements OnInit {
 
+  albumIdList = [];
+
   public params;
   searchData: any = {};
 
-  constructor( private http: HttpClient, private router: Router) { 
-    console.log("#HomePage OK !!!")
+  constructor( private http: HttpClient, private router: Router, private listProducts: ListProductsService) {
+    this.getlbumIdList()
   }
 
   ngOnInit() {
     
   }
 
+  public getlbumIdList() {
+    this.listProducts.getProducts('').subscribe((data) => {
+        for (let i=0; i<data.length; i++) {
+          if(this.albumIdList.indexOf(data[i].albumId) === -1 && data[i].albumId<11) {
+            this.albumIdList.push(data[i].albumId);
+          }
+        };
+        console.log("this.albumIdList: ",this.albumIdList);
+    });
+  }
+
   onSubmit() {
-    //console.log("this.searchData: ",this.searchData);
+    //console.log("this.searchData1: ",this.searchData);
     //alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.searchData))
     this.params = new HttpParams()
     // .set('title', this.searchData.title)
     .set('albumId', this.searchData.albumId);
     var searchParams = this.params.toString();
-    console.log("pathParams: ",searchParams);
+    //console.log("pathParams2: ",searchParams);
     this.router.navigate( [ '/products', searchParams ] );
   }  
 
